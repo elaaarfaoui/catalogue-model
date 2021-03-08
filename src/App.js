5d83e5662d3ab310f7e5ac1eca7da19a480873f7
elaaarfaoui <elaa.arfaoui1@gmail.com>
2021-03-08 15:51:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+<div className="portfolio">
+        <div className="image active" style={{background: 'url(./media/4.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>Create your Account</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/2.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>You have already an account, Please log in</h3>
+              <h3 />
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/3.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>Create your Account</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/5.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>Create your Account</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/1.jpeg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>You have already an account, Please log in</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/8.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>Create your Account</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/7.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>Create your Account</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+        <div className="image" style={{background: 'url(./media/6.jpg) center/cover'}}>
+          <div className="text-container">
+            <div className="text">
+              <h3>You have already an account, Please log in</h3>
+              <p />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
